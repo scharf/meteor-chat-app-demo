27750857f5e993a0c797b25d6571b76aaad24010
Michael Scharf <github@scharf.gr>
2017-01-30 00:48:00 +0100

    STEP: add render mixin

--- meteorchat/chat-app/globals.d.ts
+++ meteorchat/chat-app/globals.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="types/meteor.d.ts" />
+declare module 'meteor/react-meteor-data' {
+    interface IReactMeteorData {
+        data: any;
+    }
+    export var ReactMeteorData: IReactMeteorData;
+    // param `component:ComponentClass<P>` causes problem if the componet
+    // when the component has `static propTypes`
+    function createContainer<P>(propsfn: (props: P) => any, component:any): React.ComponentClass<P>;
+    export { createContainer };
+}
