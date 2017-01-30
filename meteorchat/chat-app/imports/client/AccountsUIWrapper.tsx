73d8776a39a2485b08c5b23c982a8cd29a4ed434
Michael Scharf <github@scharf.gr>
2017-01-30 12:22:29 +0100

    STEP: add authentication

---
+++ meteorchat/chat-app/imports/client/AccountsUIWrapper.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import * as ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
+import { Accounts } from 'meteor/accounts-base';
+
+Accounts.ui.config({
+    //  'USERNAME_AND_EMAIL', 'USERNAME_AND_OPTIONAL_EMAIL',
+    //  'USERNAME_ONLY', or 'EMAIL_ONLY'
+    passwordSignupFields: 'USERNAME_ONLY',
+});
+
+export default class AccountsUIWrapper extends React.Component<void,void> {
+    private view:any
+    componentDidMount() {
+        // Use Meteor Blaze to render login buttons
+        this.view = Blaze.render((Template as any).loginButtons,
+            ReactDOM.findDOMNode(this.refs['container']));
+    }
+    componentWillUnmount() {
+        // Clean up Blaze view
+        Blaze.remove(this.view);
+    }
+    render() {
+        // Just render a placeholder container that will be filled in
+        return <span ref="container"/>;
+    }
+}
--- meteorchat/chat-app/imports/client/TopBar.tsx
+++ meteorchat/chat-app/imports/client/TopBar.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { MenuItem, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import AccountsUIWrapper from "./AccountsUIWrapper";
 
 interface NavigationProperties {
     fixedTop?:boolean;
@@ -7,26 +8,23 @@ interface NavigationProperties {
 
 export class TopBar extends React.Component<NavigationProperties,void> {
 
-    render() {
+    render () {
         return (
             <Navbar fixedTop={this.props.fixedTop}>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <a href="#">React-Bootstrap</a>
+                        <a href="#">React Bootstrap</a>
                     </Navbar.Brand>
+                    <Navbar.Toggle />
                 </Navbar.Header>
+                <Navbar.Collapse>
 
-                <Nav>
-                    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                        <MenuItem eventKey={3.1}>Action</MenuItem>
-                        <MenuItem eventKey={3.2}>Another action</MenuItem>
-                        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                        <MenuItem divider/>
-                        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                    </NavDropdown>
-                </Nav>
+                    <Nav>
+                        <NavItem onClick={()=>alert('aha')}>Add Group</NavItem>
+                        <NavItem eventKey={1} href="#"><AccountsUIWrapper /></NavItem>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
-
         );
     }
 }
